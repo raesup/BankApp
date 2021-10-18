ed3a48a0447baff712fca1c8940ad4d4fd679b66
unknown <raesupkim@gmail.com>
2021-10-18 13:58:43 -0600

    balance displaying

--- script.js
+++ script.js
@@ -60,3 +60,20 @@ const inputTransferAmount = document.querySelector('.form__input--amount');
 const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
+
+const displayMovements = function(movements) {
+  containerMovements.innerHTML = '';
+
+  movements.forEach((movement, i) => {
+    const type = movement > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+        <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${i+1} ${type}</div>
+          <div class="movements__value">${movement}$</div>
+        </div>
+    `
+    containerMovements.insertAdjacentHTML('afterbegin', html)
+  })
+}
+
+displayMovements(account1.movements);
